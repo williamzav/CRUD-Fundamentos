3d73c65d68d301b68d749bfd2513d68e09080af2
Irving-Vaz <pressivazquez@gmial.com>
2026-02-18 21:54:07 -0600

    Irving agrego el index y estructura de la base de datos

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 
+session_start();
 
-?>
+require_once __DIR__ . '/app/controllers/AlumnoController.php';
+
+$action = $_GET['action'] ?? 'index';
+
+$controller = new AlumnoController();
+
+switch ($action) {
+    case 'create':
+        $controller->create();
+        break;
+    case 'store':
+        $controller->store();
+        break;
+    case 'edit':
+        $controller->edit();
+        break;
+    case 'update':
+        $controller->update();
+        break;
+    case 'delete':
+        $controller->delete();
+        break;
+    default:
+        $controller->index();
+        break;
+}
